1a7a215ec0a7ae3a238aa07ea3ea469aa3ace7b2
Your Name <your@email.com>
2025-09-09 14:14:39 +1000

    Setup on new MacBook

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900 text-white min-h-screen`}
